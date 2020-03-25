c611b8d79ebfe26fc60a2211883860951d8c5f58
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-03-25 15:13:58 +0100

    Seance 7 : Modification du composant Hello pour cliquer

--- src/hello/Hello.js
+++ src/hello/Hello.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react';
 import './Hello.css';
 
+
+           
+
 class Hello extends Component {
     
+    hello(){
+        alert("Hello à "+this.props.prenom+" "+this.props.nom+" à "+this.props.age+" ans");
+    }
+
     render(){
         var nom = this.props.nom;
         var prenom = this.props.prenom;
         var age = this.props.age;
-
-        return <div className="Hello">
+        
+        return <div onClick={()=>this.hello()} className="Hello">
                     <h1>Hello à {prenom} {nom} à {age} ans</h1>
                 </div>;
     }

2caf249e7a68b89541a8d445c36fef7ee33fefab
guillaume.artignan@gmail.com <guillaume.artignan@gmail.com>
2020-03-30 15:10:46 +0200

    Séance 9 : Composant Vignette

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './hello/Hello'
 import Manga from './manga/Manga'
+import Vignette from './vignette/Vignette'
 
 class App extends Component {
   render() {
@@ -14,6 +15,8 @@ class App extends Component {
           <Hello nom="GAILLARD" prenom="Loan" age="17"></Hello>
           <Hello nom="NAYER" prenom="Jalil" age="17"></Hello>
           <Hello nom="GRIFFOULIERE" prenom="Léo" age="17"></Hello>
+          <Vignette nom="Son Goku" nomOrigine="孫悟空" origine="Saiyan" image="https://i.pinimg.com/originals/3b/7a/9b/3b7a9b76d75738cb83dd8e984a0fb44a.png" ></Vignette>
+          <Vignette nom="Son Gohan" nomOrigine="孫悟飯" origine="Saiyan et Terrien" image="https://vignette.wikia.nocookie.net/dragonball/images/b/bc/Adult_gohan.png/revision/latest?cb=20170601162208&path-prefix=fr" ></Vignette>
       </div>
     );    
   }
---
+++ src/vignette/Vignette.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './Vignette.css';
+
+class Vignette extends Component {
+    
+    render(){
+        var style = {backgroundImage: "url('"+this.props.image+"')"}
+        return <div className="Vignette">
+                    <div style={style} className="image" ></div>
+                    <div className="description">
+                        <h2><b>Nom :</b> {this.props.nom}</h2>
+                        <h2><b>Nom d'origine :</b> {this.props.nomOrigine}</h2>
+                        <h2><b>Origine :</b> {this.props.origine}</h2>
+                    </div>
+                </div>;
+    }
+}
+
+export default Vignette;
